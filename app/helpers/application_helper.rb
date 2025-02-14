c7c755f73bfe8d22229e82a360a206ccb8a255f0
devmurariror <muraree38kumar2@gmail.com>
2025-02-14 17:51:50 +0530

    add views for project controller actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def user_from_version(version)
+    begin
+      User.find_by(id: version.whodunnit)
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
+  end
+
+  def user_email_from_version(version)
+    user = user_from_version(version)
+    user&.email || "Unknown User"
+  end
 end
